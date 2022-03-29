69b904fc572abbe4e2519de40dddff3a0cbbf353
HusainShehabi <shehabi.hussain@gmail.com>
2022-03-29 16:56:58 +0300

    edited REDIRECT_URI

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   var scope = 'user-top-read';
 
   const CLIENT_ID = "ca2a7b906f7249369617fe8577278df7"
-  const REDIRECT_URI = "http://localhost:3000"
+  const REDIRECT_URI = "https://spotify-tracks-retrieve.netlify.app"
   const  AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
   const [token, setToken] = useState("")
