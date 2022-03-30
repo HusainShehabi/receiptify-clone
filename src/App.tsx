e0fe17e7877498e826eab5c17b1cc294bb17c8db
HusainShehabi <shehabi.hussain@gmail.com>
2022-03-30 15:47:47 +0300

    redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,57 @@
-import { useEffect, useState } from 'react';
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import SpotifyGetTopSongs from './components/SpotifyGetTopSongs';
+
+import { url } from "inspector";
+import { useEffect, useState } from "react";
+import { Button } from "ui-neumorphism";
+import "./App.css";
+import SpotifyGetTopSongs from "./components/SpotifyGetTopSongs";
+
 
 function App() {
-  var scope = 'user-top-read';
+  var scope = "user-top-read";
 
-  const CLIENT_ID = "ca2a7b906f7249369617fe8577278df7"
+  const CLIENT_ID = "ca2a7b906f7249369617fe8577278df7";
   const REDIRECT_URI = "https://spotify-tracks-retrieve.netlify.app" //live
-  //const REDIRECT_URI = "http://localhost:3000" //local
-  const  AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
-  const [token, setToken] = useState("")
+  //const REDIRECT_URI = "http://localhost:3000"; //local
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    const hash = window.location.hash
-    let token:any = window.localStorage.getItem("token")
+    const hash = window.location.hash;
+    let token: any = window.localStorage.getItem("token");
 
     if (!token && hash) {
-      
-      token = hash.substring(1).split("&")?.find((elem: string) => elem.startsWith("access_token="))?.split("=")[1]
-
-      window.location.hash = ""
-      window.localStorage.setItem("token", token)
-      
+      token = hash
+        .substring(1)
+        .split("&")
+        ?.find((elem: string) => elem.startsWith("access_token="))
+        ?.split("=")[1];
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
     }
-
-    setToken(token)
-
-   
-  }, [])
+    setToken(token);
+  }, []);
 
   const logout = () => {
-  setToken("")
-  window.localStorage.removeItem("token")
-  }
-  
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Your Top Tracks v0.1</h1>
-        <h3>Made By Husain Ebrahim</h3>
-          {token ? 
-          <SpotifyGetTopSongs/>  
+        <h1>Generate Your Top Tracks</h1>
+
+          {token ?
+          <SpotifyGetTopSongs/>
 
           : <h2>Please Login</h2>
         }
         {!token ?
         <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&scope=${scope}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
          to Spotify</a>
-        : <button onClick={logout}>Logout</button>}
+        : <Button onClick={logout}>Logout</Button>}
+        <br/>
       </header>
     </div>
   );
--- src/components/SpotifyGetTopSongs.tsx
+++ src/components/SpotifyGetTopSongs.tsx
@@ -1,86 +1,119 @@
-import { Card, CardHeader, Avatar, IconButton, CardMedia, CardContent, Typography, CardActions, Collapse, styled, Box, Grid, Paper, Button } from "@mui/material";
-import { red } from "@mui/material/colors";
+import styled from "@emotion/styled";
+import { Box, Grid } from "@mui/material";
 import axios from "axios";
 import React, { useState } from "react";
+import {
+  Body2,
+  Button,
+  Card,
+  CardAction,
+  CardContent,
+  CardMedia,
+  Subtitle2,
+} from "ui-neumorphism";
+import "ui-neumorphism/dist/index.css";
 
 const SpotifyGetTopSongs = () => {
-    const [token, setToken] = useState<string | null>("");
-    const [data, setData] = useState<any>({});
+  const [token, setToken] = useState<string | null>("");
+  const [data, setData] = useState<any>({});
 
+  const TOP_ARTISTS_LONGTERM_ENDPOINT =
+    "https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=10&offset=0";
+  const TOP_ARTISTS_MEDIUMTERM_ENDPOINT =
+    "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=10&offset=0";
+  const TOP_ARTISTS_SHORTTERM_ENDPOINT =
+    "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10&offset=0";
 
-    const TOP_ARTISTS_MEDIUMTERM_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=10&offset=0"
-    const TOP_ARTISTS_SHORTTERM_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10&offset=0"
-
-    React.useEffect(() => {
-        if(localStorage.getItem("token")) {
-            setToken(localStorage.getItem("token"));
-        }
-    }, []);
-
-    const handleGetTopTracks_MedTerm = async (e: any) => {
-        axios.get(TOP_ARTISTS_MEDIUMTERM_ENDPOINT, {
-            headers: {
-                Authorization: "Bearer " + token
-            },
-        }).then((response) => {
-            setData(response.data);
-        })
-        .catch((error) => {console.log(error)})
+  React.useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
     }
+  }, []);
 
-    const handleGetTopTracks_ShortTerm = async (e: any) => {
-      axios.get(TOP_ARTISTS_SHORTTERM_ENDPOINT, {
-          headers: {
-              Authorization: "Bearer " + token
-          },
-      }).then((response) => {
-          setData(response.data);
+  const handleGetTopTracks_LongTerm = async (e: any) => {
+    axios
+      .get(TOP_ARTISTS_LONGTERM_ENDPOINT, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
       })
-      .catch((error) => {console.log(error)})
-  }
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+  const handleGetTopTracks_MedTerm = async (e: any) => {
+    axios
+      .get(TOP_ARTISTS_MEDIUMTERM_ENDPOINT, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  const handleGetTopTracks_ShortTerm = async (e: any) => {
+    axios
+      .get(TOP_ARTISTS_SHORTTERM_ENDPOINT, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  return(
+  return (
     <>
+  <br />
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-        <Button variant="contained" onClick={handleGetTopTracks_MedTerm}>Last 6 Months</Button>
+      <Grid container spacing={4}>
+        <Grid item xs={4}>
+        <Button onClick={handleGetTopTracks_MedTerm}>Last 6 Months</Button>
         </Grid>
-        <Grid item xs={6}>
-        <Button variant="contained" onClick={handleGetTopTracks_ShortTerm}>Last 4 Weeks</Button>
+        <Grid item xs={5}>
+        <Button onClick={handleGetTopTracks_ShortTerm}>Last 4 Weeks</Button>
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={1}>
+        <Button onClick={handleGetTopTracks_LongTerm}>All Time</Button>
         </Grid>
       </Grid>
     </Box>
-    
-    
     <br />
-    {data.items ? data.items.map((item: any) => <div key={data.id}>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        title={item.name}
-        subheader={item.artists[0].name}
-      />
-      <CardMedia
-        component="img"
+  
+      {data.items
+        ? data.items.map((item: any) => (
+            <div key={data.id}>
+              <Card width={350}>
+                <CardMedia height={350} dark src={item.album.images[0].url} />
+                <CardContent>
+                  <Subtitle2 secondary style={{ marginBottom: "4px" }}>
+                    {item.name}
+                  </Subtitle2>
+                  <Body2>{item.artists[0].name}</Body2>
+                </CardContent>
+                <CardAction>
+                </CardAction>
+              </Card>
 
-        image={item.album.images[0].url}
-      />
-    </Card>
-    <br />
-    </div>) : null}
+              <br />
+            </div>
+          ))
+        : null}
     </>
-  )
+  );
 };
 
 export default SpotifyGetTopSongs;
